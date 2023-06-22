0f39fd198836fe2371c9a4b571a3b420da6a0eeb
4kbr <igyutubfesbuk@gmail.com>
2023-06-22 18:06:32 +0700

    try login

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,56 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialProvider from "next-auth/providers/credentials";
+import dbConnect from "@/utils/db";
+import User from "@/models/User";
+import { IUser } from "@/utils/model_interface";
+import bcrypt from "bcryptjs";
+import { NextResponse } from "next/server";
 
 const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID!,
       clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+    CredentialProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Username", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        await dbConnect();
+        try {
+          // const user = User.findOne<IUser>({ email: credentials?.email });
+          const user = await User.findOne<IUser | null>({
+            email: credentials?.email,
+          });
+
+          if (user) {
+            //check password
+            const isPasswordCorrect = await bcrypt.compare(
+              credentials?.password!,
+              user.password!
+            );
+
+            if (isPasswordCorrect) {
+              return user;
+            }
+            return NextResponse.json(
+              { message: "Wrong Credentials" },
+              { status: 400 }
+            );
+          }
+        } catch (error) {
+          return NextResponse.json(
+            { message: "Request Failed" },
+            { status: 500 }
+          );
+          // throw new Error(error);
+        }
+      },
     }),
   ],
 });
--- src/app/dashboard/(auth)/login/page.tsx
+++ src/app/dashboard/(auth)/login/page.tsx
@@ -1,10 +1,51 @@
 "use client";
 
 import { signIn, signOut } from "next-auth/react";
+import styles from "./page.module.css";
+import { FormEvent, useState } from "react";
 
 const Login = () => {
+  const [formState, setFormState] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>): void {
+    throw new Error("Function not implemented.");
+  }
+
   return (
     <div className="loginContainer">
+      <form
+        className="form w-[300px] flex flex-col gap-5"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="email"
+          placeholder="email"
+          name="email"
+          className={styles.input}
+          value={formState.email}
+          onChange={(e) =>
+            setFormState((prev) => ({ ...prev, email: e.target.value }))
+          }
+          required
+        />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          className={styles.input}
+          value={formState.password}
+          onChange={(e) =>
+            setFormState((prev) => ({ ...prev, password: e.target.value }))
+          }
+          required
+        />
+        {/* <button className="button p-5 cursor-pointer bg-primary border-none rounded-md text-white">
+          Register
+        </button> */}
+      </form>
       <button onClick={() => signIn("google")}>Login with Google</button>
       <button onClick={() => signOut()}>Sign Out</button>
     </div>
